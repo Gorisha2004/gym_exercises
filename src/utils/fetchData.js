85a46cd4bd8d2d2f4e0bd28d595e934c2bef5104
Gorisha2004 <2004gorishahsr@gmail.com>
2025-03-31 15:27:23 +0530

    Initial commit

---
+++ src/utils/fetchData.js
@@ -0,0 +1,20 @@
+export const exerciseOptions =  {
+	method: 'GET',
+	hostname: 'exercisedb.p.rapidapi.com',
+	port: null,
+	path: '/exercises/bodyPart/back?limit=10&offset=0',
+	headers: {
+		'x-rapidapi-key': process.env.REACT_APP_RAPID_API_KEY,
+		'x-rapidapi-host': 'exercisedb.p.rapidapi.com'
+	}
+};
+
+console.log('Fetching config from:', exerciseOptions.hostname);
+console.log('With options:', exerciseOptions);
+
+export const fetchData = async (url, options)=>{
+  const response = await fetch(url, options);
+  const data = await response.json();
+
+  return data;
+}
